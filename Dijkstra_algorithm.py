4965c7222530e26afd30a363e488126213de9485
Vovka <59031665+DoroninDobro@users.noreply.github.com>
2020-04-06 18:48:41 +0300

    Create Dijkstra_algorithm.py

---
+++ Dijkstra_algorithm.py
@@ -0,0 +1,33 @@
+costs = {}
+costs['a'] = 6
+costs['b'] = 2
+costs['fin'] = float('inf')
+
+parents = {}
+parents['a'] = 'start'
+parents['b'] = 'start'
+parents['fin'] = None
+
+processed = []
+
+def find_lowest_cost_node(costs):
+    lowest_cost = float('inf')
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+node = find_lowest_cost_node(costs)
+while node is not None:
+    cost = costs[node]
+    neighbours = graph[node]
+    for n in neighbours.keys():
+        new_cost = cost + neighbours[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node)
+    node = find_lowest_cost_node(costs)
